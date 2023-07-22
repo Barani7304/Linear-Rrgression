80932528d4a482e0abe7b84f5980b249ce1b4600
Barani7304 <ilangobarani2004@gmail.com>
2023-07-22 14:28:07 +0530

    EDA

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-# Gives us the erroe message
+# Gives us the error message
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
     filename=exc_tb.tb_frame.f_code.co_filename
